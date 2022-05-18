d2d9d0569f04a27bb163d35b0949ed037ccfcf74
Lucas Lara <lucasml@live.com>
2022-05-18 18:59:06 -0300

    Format typescript lint

--- src/api/marvel/interface.tsx
+++ src/api/marvel/interface.tsx
@@ -1,51 +1,51 @@
 interface Url {
-type: string
-url: string
+  type: string
+  url: string
 }
 interface Image {
-path: string
-extension: string
+  path: string
+  extension: string
 }
 interface ComicSummary {
-resourceURI: string
-name: string
+  resourceURI: string
+  name: string
 }
 interface ComicList {
-available: number
-returned: number
-collectionURI: string
-items: ComicSummary[]
+  available: number
+  returned: number
+  collectionURI: string
+  items: ComicSummary[]
 }
 interface StorySummary {
-resourceURI: string
-name: string
-type: string
+  resourceURI: string
+  name: string
+  type: string
 }
 interface StoryList {
-available: number
-returned: number
-collectionURI: string
-items: StorySummary[]
+  available: number
+  returned: number
+  collectionURI: string
+  items: StorySummary[]
 }
 interface EventSummary {
-resourceURI: string
-name: string
+  resourceURI: string
+  name: string
 }
 interface EventList {
-available: number
-returned: number
-collectionURI: string
-items: EventSummary[]
+  available: number
+  returned: number
+  collectionURI: string
+  items: EventSummary[]
 }
 interface SeriesSummary {
-resourceURI: string
-name: string
+  resourceURI: string
+  name: string
 }
 interface SeriesList {
-available: number
-returned: number
-collectionURI: string
-items: SeriesSummary[]
+  available: number
+  returned: number
+  collectionURI: string
+  items: SeriesSummary[]
 }
 
 interface Character {
@@ -60,7 +60,7 @@ interface Character {
   stories: StoryList
   events: EventList
   series: SeriesList
-  }
+}
 interface CharacterDataContainer {
   offset: number
   limit: number
@@ -69,11 +69,11 @@ interface CharacterDataContainer {
   results: Character[]
 }
 export interface CharacterDataWrapper {
-code: number
-status: string
-copyright: string
-attributionText: string
-attributionHTML: string
-data: CharacterDataContainer
-etag: string
+  code: number
+  status: string
+  copyright: string
+  attributionText: string
+  attributionHTML: string
+  data: CharacterDataContainer
+  etag: string
 }
